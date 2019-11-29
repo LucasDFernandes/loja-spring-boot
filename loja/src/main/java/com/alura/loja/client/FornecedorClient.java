c109ba44f08dabe27bb4362d948c8f0f99b6b787
lucas.fernandes <lucas.fernandes@SPTIA-NT8912.anbima.com.int>
2019-11-29 09:43:54 -0300

    Mudança de RestTemplate para FeighClient

---
+++ loja/src/main/java/com/alura/loja/client/FornecedorClient.java
@@ -0,0 +1,15 @@
+package com.alura.loja.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.alura.loja.controller.dto.InfoFornecedorDTO;
+
+@FeignClient("fornecedor")
+public interface FornecedorClient {
+
+	@RequestMapping("/info/{estado}")
+	InfoFornecedorDTO getInfoPorEstado(@PathVariable String estado);
+
+}
--- loja/src/main/java/com/alura/loja/service/CompraService.java
+++ loja/src/main/java/com/alura/loja/service/CompraService.java
@@ -1,11 +1,9 @@
 package com.alura.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import com.alura.loja.client.FornecedorClient;
 import com.alura.loja.controller.dto.CompraDTO;
 import com.alura.loja.controller.dto.InfoFornecedorDTO;
 
@@ -13,13 +11,11 @@ import com.alura.loja.controller.dto.InfoFornecedorDTO;
 public class CompraService {
 
 	@Autowired
-	private RestTemplate client;
+	private FornecedorClient fornecedorClient;
 
 	public void realizaCompra(CompraDTO compra) {
-		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(
-				"http://fornecedor/info/" + compra.getEndereco().getEstado(), HttpMethod.GET, null,
-				InfoFornecedorDTO.class);
-		System.out.println(exchange);
+		InfoFornecedorDTO infoPorEstado = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		System.out.println(infoPorEstado);
 	}
 
 }
